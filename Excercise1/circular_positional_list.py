3c2231076ba58de0655030d78e8a8f292254f9fb
Mario <mariocerino83@gmail.com>
2019-10-31 18:03:28 +0100

    reverse and bug fixed

--- Excercise1/circular_positional_list.py
+++ Excercise1/circular_positional_list.py
@@ -6,6 +6,8 @@ class CircularPositionalList(PositionalList):
         super().__init__()
         # Conta gli elementi non ordinati
         self._count_not_sorted = 0
+        # Utilizziamo la proprietà is_reversed per indicare che la lista è reversed rispetto alla struttura interna
+        self._is_reversed = False
 
     class _Node(PositionalList._Node):
 
@@ -14,25 +16,68 @@ class CircularPositionalList(PositionalList):
             self._sorted_left = True
             self._sorted_right = True
 
+    # ## To implement reverse in O(1) is needed to override first and last methods ## #
+    def first(self):
+        if self._is_reversed:
+            self._is_reversed=False
+            p = super().last()
+            self._is_reversed = True
+            return p
+        return super().first()
+
+    def last(self):
+        if self._is_reversed:
+            self._is_reversed = False
+            p = super().first()
+            self._is_reversed = True
+            return p
+        return super().last()
+
     def before(self, p):
+        #Se è reversed, il metodo before sarebbe il metodo after
+        if self._is_reversed:
+            #Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p2 = self.after(p)
+            self._is_reversed = True
+            return p2
+
         node = self._validate(p)
-        if node is self._header:
-            return self._make_position(node._prev)
-        return p
+        before = node._prev
+        if before is self._header:
+            return self._make_position(before._prev)
+        return super().before(p)
 
     def after(self, p):
+        #Se è reversed, il metodo before sarebbe il metodo after
+        if self._is_reversed:
+            #Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p2 = self.before(p)
+            self._is_reversed = True
+            return p2
         node = self._validate(p)
-        if node is self._trailer:
-            return self._make_position(node._next)
-        return p
+        next = node._next
+        if next is self._trailer:
+            return self._make_position(next._next)
+        return super().after(p)
 
     def is_empty(self):
         return self._size==0
 
     def is_sorted(self):
+        if self._is_reversed:
+            return self._count_not_sorted==(2*self._size-2)
         return self._count_not_sorted==0
 
     def add_first(self, e):
+        if self._is_reversed:
+            #Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p = self.add_last(e)
+            self._is_reversed = True
+            return p
+
         p = super().add_first(e)
         node = self._validate(p)
         self._trailer._next = node
@@ -46,6 +91,13 @@ class CircularPositionalList(PositionalList):
         return p
 
     def add_last(self, e):
+        if self._is_reversed:
+            #Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p = self.add_first(e)
+            self._is_reversed = True
+            return p
+
         p = super().add_last(e)
         node = self._validate(p)
         self._header._prev = node
@@ -59,45 +111,55 @@ class CircularPositionalList(PositionalList):
         return p
 
     def delete(self, p):
-
+        node_prev, node_next = None, None
         p_prev = self.before(p)
-        if p_prev is not None:
-            node_prev = self._validate(p_prev)
         p_next = self.after(p)
-        if p_next is not None:
-            node_next = self._validate(p_next)
-        node = self._validate(p)
-        el = node._element
-        super().delete(p)
 
+        node = self._validate(p)
+        # aggiorno i valori sorted_left e sorted_right
         if node._sorted_left == False:
             self._count_not_sorted -= 1
         if node._sorted_right == False:
             self._count_not_sorted -= 1
+        #Poi posso eliminarlo
+        el = node._element
+        super().delete(p)
+
+
+        if self.is_empty(): #Nodo da cancellare era l'unico
+            return el
+
+        if p_prev is not None: # La position precedente il nodo da cancellare non racchiudeva header
+            node_prev = self._validate(p_prev)
+        if p_next is not None:
+            node_next = self._validate(p_next)
+
 
         # Check i nodi rimasti
         # Verifico il caso in cui elimino un elemento in mezzo
-        if node_prev is not self._header:
+        if node_prev is not None:  # Se il precedente del nodo eliminato non è header (il nodo da eliminare NON stava in testa)
 
-            if node_prev._next is not None:
+            if node_prev._next is not self._trailer:  # Il nodo rimasto non è l'ultimo
                 if node_prev._element > node_prev._next._element:
                     if node_prev._sorted_right:
                         node_prev._sorted_right = False
                         self._count_not_sorted += 1
-            else:
+            else:  # Il nodo rimasto è nell'ultima position
+                self._header._prev = node_prev
                 if not node_prev._sorted_right:
                     node_prev._sorted_right = True
                     self._count_not_sorted -= 1
 
-        if node_next is not self._trailer:
+        if node_next is not None:
 
-            if node_next._prev is not None: # Il nodo cancellato non era in testa
+            if node_next._prev is not self._header: # Il nodo cancellato non era in testa
                 if node_next._element < node_next._prev._element:
                     if node_next._sorted_left:
                         node_prev._sorted_left = False
                         self._count_not_sorted += 1
 
-            else: #Il nodo cancellato era in testa
+            else: # Il nodo cancellato era in testa (adesso è il next che sta in testa)
+                self._trailer._next = node_next
                 if not node_next._sorted_left:
                     node_next._sorted_left = True
                     self._count_not_sorted -= 1
@@ -105,27 +167,45 @@ class CircularPositionalList(PositionalList):
         return el
 
     def add_before(self,p,e):
+        if self._is_reversed:
+            # Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p = self.add_after(p, e)
+            self._is_reversed = True
+            return p
+
         node = self._validate(p)
         if node is self._header._next:
             return self.add_first(e)
         predecessor = self.before(p)
-        successor = self.after(p)
+        successor = p
         node = self._insert_between_rebalance(e, predecessor, successor)
         return self._make_position(node)
 
     def add_after(self,p,e):
+        if self._is_reversed:
+            # Dato che ogni metodo controerà se è reversed, per far eseguire after è necessario settare is_reversed a False
+            self._is_reversed = False
+            p = self.add_before(p, e)
+            self._is_reversed = True
+            return p
+
         node = self._validate(p)
         if node is self._trailer._prev:
             return self.add_last(e)
-        predecessor = self.before(p)
+        predecessor = p
         successor = self.after(p)
         node = self._insert_between_rebalance(e, predecessor, successor)
         return self._make_position(node)
 
     def _insert_between_rebalance(self, e, predecessor, successor):
+        """This method perform the updating of the sorted_right and
+           sorted_left properties of the nodes involved in the insert
+        """
         node_prev = self._validate(predecessor)
         node_next = self._validate(successor)
-        node = super()._insert_between(e,predecessor,successor)
+        p = super()._insert_between(e,node_prev,node_next)
+        node = self._validate(p)
 
         if e < node_prev._element:
             if node_prev._sorted_right == True:
@@ -143,6 +223,8 @@ class CircularPositionalList(PositionalList):
 
         return node
 
+    def reverse(self):
+        self._is_reversed = not self._is_reversed
 
 
 
--- Excercise1/test_circular_positional_list.py
+++ Excercise1/test_circular_positional_list.py
@@ -7,6 +7,23 @@ class TestCircularPositionalList(unittest.TestCase):
     def setUp(self):
         self._l = CircularPositionalList()
 
+    def test_after(self):
+        p1 = self._l.add_last(1)
+        p2 = self._l.add_last(2)
+        p3 = self._l.add_last(3)
+        self.assertEqual(self._l.after(self._l.first()).element(), 2)
+        self.assertEqual(self._l.after(p2).element(), 3)
+        self.assertEqual(self._l.after(p3).element(), 1)
+
+    def test_before(self):
+        p1 = self._l.add_last(1)
+        p2 = self._l.add_last(2)
+        p3 = self._l.add_last(3)
+        self.assertEqual(self._l.before(self._l.last()).element(), 2)
+        self.assertEqual(self._l.before(p2).element(), 1)
+        self.assertEqual(self._l.before(p1).element(), 3)
+
+
     def test_add_first(self):
         e = 10
         #Singolo inserimento
@@ -28,18 +45,81 @@ class TestCircularPositionalList(unittest.TestCase):
         p = self._l.add_before(succ,9)
         self.assertEqual(self._l.first().element(),p.element())
         self.assertTrue(self._l.is_sorted())
+        prev = self._l.add_before(p, 8)
+        self.assertEqual(self._l.first().element(), 8)
+        self.assertEqual(self._l.after(self._l.first()).element(), 9)
 
     def test_add_after(self):
         succ = self._l.add_first(9)
         p = self._l.add_after(succ,10)
         self.assertEqual(self._l.last().element(),p.element())
         self.assertTrue(self._l.is_sorted())
+        prev = self._l.add_after(p, 11)
+        self.assertEqual(self._l.last().element(), 11)
+        self.assertEqual(self._l.before(self._l.last()).element(), 10)
 
     def test_delete(self):
         self._l.add_last(8)
         self._l.add_last(9)
         self._l.add_last(11)
+        # 8 -> 9 -> 10
         e = self._l.delete(self._l.last())
+        # 8 -> 9
         self.assertEqual(e,11)
         self.assertTrue(self._l.is_sorted())
+        e = self._l.delete(self._l.first())
+        # 9
+        self.assertEqual(e,8)
+        self.assertTrue(self._l.is_sorted())
+
+        e = self._l.delete(self._l.first())
+        # Vuota
+        self.assertEqual(e,9)
+        self.assertTrue(self._l.is_sorted())
+
+    def test_reverse(self):
+        self._l.add_last(3)
+        self._l.add_last(4)
+        self._l.add_last(5)
+        # 3 -> 4 -> 5
+        self._l.reverse()
+        # 5 -> 4 -> 3
+        self.assertTrue(self._l._is_reversed)
+        self.assertEqual(self._l.first().element(), 5)
+        self.assertEqual(self._l.last().element(), 3)
+        self.assertEqual(self._l.after(self._l.first()).element(), 4)
+        self.assertEqual(self._l.before(self._l.last()).element(), 4)
+        self.assertFalse(self._l.is_sorted())
+
+        p = self._l.add_last(1)
+        # 5 -> 4 -> 3 -> 1
+        self.assertEqual(self._l.first().element(), 5)
+        self.assertEqual(self._l.last().element(), 1)
+        self.assertEqual(self._l.after(self._l.first()).element(), 4)
+        self.assertEqual(self._l.before(p).element(), 3)
+
+        p2 = self._l.add_before(p, 2)
+
+        # 5 -> 4 -> 3 -> 2 -> 1
+        self.assertEqual(self._l.last().element(), 1)
+        self.assertEqual(self._l.before(p2).element(), 3)
+        self.assertEqual(self._l.before(self._l.last()).element(), 2)
+
+        self._l.reverse()
+        # 1 -> 2 -> 3 -> 4 -> 5
+        self.assertFalse(self._l._is_reversed)
+        self._l.add_first(0.5)
+        self.assertEqual(self._l.first().element(), 0.5)
+        self.assertEqual(self._l.last().element(), 5)
+        self.assertTrue(self._l.is_sorted())
+
+    def test_is_sorted(self):
+        self._l.add_last(10)
+        self._l.add_last(9)
+        self._l.add_last(8)
+        self.assertFalse(self._l.is_sorted())
+        self._l.reverse()
+        self.assertTrue(self._l.is_sorted())
+
+
 
